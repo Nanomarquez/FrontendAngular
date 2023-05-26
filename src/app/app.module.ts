bac464b8f5826bc7e49f0d2d068c6a7be386057b
Federico Marquez <fmarquez@aimonkey.io>
2023-05-26 00:55:38 -0300

    testAngular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,58 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//para trabajar con reactive forms
+import { ReactiveFormsModule } from '@angular/forms';
+//para trabajar con peticiones http
+import { HttpClientModule } from '@angular/common/http';
+//tablas de material y pagination
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+//forms de material
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MomentDateModule } from '@angular/material-moment-adapter';
+
+//alertas material
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+//iconos material
+import { MatIconModule } from '@angular/material/icon';
+
+//modales material
+import { MatDialogModule } from '@angular/material/dialog';
+
+//grillas material
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MomentDateModule,
+    MatSnackBarModule,
+    MatIconModule,
+    MatDialogModule,
+    MatGridListModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
